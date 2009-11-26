6da78f19b53bc0e3ea1a638908d17e82a4fc4f6c
Doug Hellmann <doug.hellmann@gmail.com>
2009-11-26 17:03:03 -0500

    support date ranges in the diary

--- ical2org/tests/test_diary.py
+++ ical2org/tests/test_diary.py
@@ -50,3 +50,17 @@ def test_format_time_range():
     text = format_for_diary(e)
     assert text == '11/26/09 09:05-13:25 This is a note', text
     return
+
+def test_format_date_range():
+    c = vobject.iCalendar()
+    e = c.add('vevent')
+    e.add('summary').value = "This is a note"
+    start = e.add('dtstart')
+    utc = vobject.icalendar.utc
+    start.value = datetime.datetime(2009, 11, 26, 9, 5, tzinfo = utc)
+    end = e.add('dtend')
+    end.value = datetime.datetime(2009, 12, 26, 13, 25, tzinfo = utc)
+    print end.value
+    text = format_for_diary(e)
+    assert text == '%%(diary-block 11 26 2009 12 26 2009) This is a note', text
+    return
