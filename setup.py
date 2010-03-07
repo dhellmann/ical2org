2090c0bdbcd8fec905243b33ea2f448f6ca0a3b8
Doug Hellmann <doug.hellmann@gmail.com>
2010-03-07 08:53:46 -0500

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 PROJECT = 'ical2org'
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 try:
     long_description = open('README', 'rt').read()
